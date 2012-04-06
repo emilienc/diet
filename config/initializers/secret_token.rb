cad542b37093305a687eb3bdfcac1e079947a0cc
emilien <emilien.charbonnier@cegetel.net>
2012-04-06 13:12:35 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diet::Application.config.secret_token = 'f8f84dc40ee45fe25ea463f78465b5b0a6fd46d70ad7fc42cee473643524277311c611ff28ee397f11769baad8279b35dc223fae6f58403ece7861c0f0eee371'
